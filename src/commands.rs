5cff36e406708c9ba15441e18fbb5bde8f2525d0
Samrat Man Singh <samratmansingh@gmail.com>
2019-06-15 10:35:48 +0000

    Make each command a fn taking in a context(env, stdin, etc)
    These functions return an error string which is displayed by
    main.

---
+++ src/commands.rs
@@ -0,0 +1,188 @@
+use std::path::{Path, PathBuf};
+use std::io::{self, Read, Write};
+use std::fs;
+use std::collections::HashMap;
+
+use crate::database::{Object, Blob, Tree, Entry};
+use crate::commit::{Author, Commit};
+use crate::repository::Repository;
+
+pub struct CommandContext<I,O,E>
+    where I: Read,
+          O: Write,
+          E: Write {
+    pub dir: PathBuf,
+    pub env: HashMap<String, String>,
+    pub args: Vec<String>,
+    pub stdin: I,
+    pub stdout: O,
+    pub stderr: E,
+}
+
+pub fn execute<I,O,E>(ctx: CommandContext<I,O,E>) ->Result<(), String>
+    where I: Read,
+          O: Write,
+          E: Write {
+    let command = &ctx.args[1];
+    match &command[..] {
+        "init" => init_command(ctx),
+        "commit" => commit_command(ctx),
+        "add" => add_command(ctx),
+        _ => Err(
+            format!("invalid command: {}", command)
+                ),
+    }
+}
+
+pub fn init_command<I,O,E>(mut ctx: CommandContext<I,O,E>) -> Result<(), String>
+    where I: Read,
+          O: Write,
+          E: Write {
+    let working_dir = ctx.dir;
+    let root_path = if ctx.args.len() > 2 {
+        Path::new(&ctx.args[2])
+    } else {
+        working_dir.as_path()
+    };
+    let git_path = root_path.join(".git");
+
+    for d in ["objects", "refs"].iter() {
+        fs::create_dir_all(git_path.join(d))
+            .expect("failed to create dir");
+    }
+
+    ctx.stdout.write_all(
+        format!("Initialized empty Jit repository in {:?}", git_path).as_bytes()
+    ).unwrap();
+    
+    Ok(())
+}
+
+pub fn commit_command<I,O,E>(mut ctx: CommandContext<I,O,E>) -> Result<(), String>
+    where I: Read,
+          O: Write,
+          E: Write {
+    let working_dir = ctx.dir;
+    let root_path = working_dir.as_path();
+    let mut repo = Repository::new(&root_path.join(".git"));
+
+    repo.index.load()
+        .expect("loading .git/index failed");
+    let entries : Vec<Entry> = repo.index.entries.iter()
+        .map(|(_path, idx_entry)| Entry::from(idx_entry))
+        .collect();
+    let root = Tree::build(&entries);
+    root.traverse(&repo.database)
+        .expect("Traversing tree to write to database failed");
+
+    let parent = repo.refs.read_head();
+    let author_name = ctx.env.get("GIT_AUTHOR_NAME")
+        .expect("GIT_AUTHOR_NAME not set");
+    let author_email = ctx.env.get("GIT_AUTHOR_EMAIL")
+        .expect("GIT_AUTHOR_EMAIL not set");
+
+    let author = Author { name: author_name.to_string(),
+                          email: author_email.to_string() };
+
+    let mut commit_message = String::new();
+    ctx.stdin.read_to_string(&mut commit_message)
+        .expect("reading commit from STDIN failed");
+
+    let commit = Commit::new(&parent, root.get_oid(), author, commit_message);
+    repo.database.store(&commit)
+        .expect("writing commit failed");
+    repo.refs.update_head(&commit.get_oid())
+        .expect("updating HEAD failed");
+    repo.refs.update_master_ref(&commit.get_oid())
+        .expect("updating master ref failed");
+
+    let commit_prefix = if parent.is_some() {
+        ""
+    } else {
+        "(root-commit) "
+    };
+
+    println!("[{}{}] {}", commit_prefix, commit.get_oid(), commit.message);
+    
+    Ok(())
+}
+
+pub fn add_command<I,O,E>(ctx: CommandContext<I,O,E>) -> Result<(), String>
+    where I: Read,
+          O: Write,
+          E: Write {
+    let working_dir = ctx.dir;
+    let root_path = working_dir.as_path();
+    let mut repo = Repository::new(&root_path.join(".git"));
+    
+    match repo.index.load_for_update() {
+        Ok(_) => (),
+        Err(ref e)
+            if e.kind() == io::ErrorKind::AlreadyExists => {
+                
+                return Err(
+                    format!("fatal: {}
+
+Another jit process seems to be running in this repository. Please make sure all processes are terminated then try again.
+
+If it still fails, a jit process may have crashed in this repository earlier: remove the .git/index.lock file manually to continue.\n",
+                        e)
+                );
+            },
+        Err(_) => {
+            return Err(
+                "fatal: could not create/load .git/index\n".to_string()
+            );
+        },
+    }
+
+    let mut paths = vec![];
+    for arg in &ctx.args[2..] {
+        let path = match Path::new(arg).canonicalize() {
+            Ok(path) => path,
+            Err(_) => {
+                repo.index.release_lock().unwrap();
+                return Err(
+                    format!("fatal: pathspec '{:}' did not match any files\n", arg)
+                );
+
+            },
+        };
+
+        for pathname in repo.workspace.list_files(&path).unwrap() {
+            paths.push(pathname);
+        }
+    }
+
+    for pathname in paths {
+        let data = match repo.workspace.read_file(&pathname) {
+            Ok(data) => data,
+            Err(ref err)
+                if err.kind() == io::ErrorKind::PermissionDenied => {
+                    repo.index.release_lock().unwrap();
+                    return Err(
+                        format!("{}
+
+fatal: adding files failed\n", err)
+                    );
+                },
+            _ => {
+                panic!("fatal: adding files failed");
+            },
+        };
+        
+        let stat = repo.workspace.stat_file(&pathname)
+            .expect("could not stat file");
+        let blob = Blob::new(data.as_bytes());
+        repo.database.store(&blob)
+            .expect("storing blob failed");
+        
+        repo.index.add(&pathname, &blob.get_oid(), &stat);
+    }
+
+    repo.index.write_updates()
+        .expect("writing updates to index failed");
+    
+    Ok(())
+
+}
--- src/main.rs
+++ src/main.rs
@@ -6,155 +6,39 @@ extern crate chrono;
 extern crate lazy_static;
 
 use std::env;
-use std::path::Path;
-use std::fs;
-use std::io::prelude::*;
-use std::io;
+use std::io::{self, Write};
+use std::collections::HashMap;
 
 mod lockfile;
 
 mod database;
-use database::{Object, Blob, Tree, Entry};
-
 mod workspace;
 mod index;
 mod refs;
-
+mod commit;
+mod repository;
 mod util;
 
-mod commit;
-use commit::{Author, Commit};
+mod commands;
+use commands::{execute, CommandContext};
 
-mod repository;
-use repository::Repository;
-
-fn main() -> std::io::Result<()> {
+fn main() {
     let args : Vec<String> = env::args().collect();
-    let command = &args[1];
-    match &command[..] {
-        "init" => {
-            let working_dir = env::current_dir()?;
-            let root_path = if args.len() > 2 {
-                Path::new(&args[2])
-            } else {
-                working_dir.as_path()
-            };
-            let git_path = root_path.join(".git");
+    let ctx = CommandContext {
+        dir: env::current_dir().unwrap(),
+        env: env::vars().collect::<HashMap<String, String>>(),
+        args,
+        stdin: io::stdin(),
+        stdout: io::stdout(),
+        stderr: io::stderr(),
+    };
 
-            for d in ["objects", "refs"].iter() {
-                fs::create_dir_all(git_path.join(d))?;
-            }
-
-            println!("Initialized empty Jit repository in {:?}", git_path);
-            
-            Ok(())
+    match execute(ctx) {
+        Ok(_) => (),
+        Err(msg) => {
+            io::stderr().write_all(msg.as_bytes())
+                .unwrap();
+            std::process::exit(128);
         },
-        "commit" => {
-            let working_dir = env::current_dir()?;
-            let root_path = working_dir.as_path();
-            let mut repo = Repository::new(&root_path.join(".git"));
-
-            repo.index.load()?;
-            let entries : Vec<Entry> = repo.index.entries.iter()
-                .map(|(_path, idx_entry)| Entry::from(idx_entry))
-                .collect();
-            let root = Tree::build(&entries);
-            root.traverse(&repo.database)?;
-
-            let parent = repo.refs.read_head();
-            let author_name = env::var("GIT_AUTHOR_NAME")
-                .expect("GIT_AUTHOR_NAME not set");
-            let author_email = env::var("GIT_AUTHOR_EMAIL")
-                .expect("GIT_AUTHOR_EMAIL not set");
-
-            let author = Author { name: author_name,
-                                  email: author_email };
-
-            let mut commit_message = String::new();
-            io::stdin().read_to_string(&mut commit_message)?;
-
-            let commit = Commit::new(&parent, root.get_oid(), author, commit_message);
-            repo.database.store(&commit)?;
-            repo.refs.update_head(&commit.get_oid())?;
-            repo.refs.update_master_ref(&commit.get_oid())?;
-
-            let commit_prefix = if parent.is_some() {
-                ""
-            } else {
-                "(root-commit) "
-            };
-
-            println!("[{}{}] {}", commit_prefix, commit.get_oid(), commit.message);
-            
-            Ok(())
-        },
-        "add" => {
-            let working_dir = env::current_dir()?;
-            let root_path = working_dir.as_path();
-            let mut repo = Repository::new(&root_path.join(".git"));
-            
-            match repo.index.load_for_update() {
-                Ok(_) => (),
-                Err(ref e)
-                    if e.kind() == io::ErrorKind::AlreadyExists => {
-                    eprintln!("fatal: {}
-
-Another jit process seems to be running in this repository. Please make sure all processes are terminated then try again.
-
-If it still fails, a jit process may have crashed in this repository earlier: remove the .git/index.lock file manually to continue.",
-                              e);
-                    std::process::exit(128);
-                    },
-                Err(_) => {
-                    eprintln!("fatal: could not create/load .git/index");
-                    std::process::exit(128);
-                },
-            }
-
-            let mut paths = vec![];
-            for arg in &args[2..] {
-                let path = match Path::new(arg).canonicalize() {
-                    Ok(path) => path,
-                    Err(_) => {
-                        eprintln!("fatal: pathspec '{:}' did not match any files", arg);
-                        repo.index.release_lock()?;
-                        std::process::exit(1);
-                    },
-                };
-
-                for pathname in repo.workspace.list_files(&path)? {
-                    paths.push(pathname);
-                }
-            }
-
-            for pathname in paths {
-                let data = match repo.workspace.read_file(&pathname) {
-                    Ok(data) => data,
-                    Err(ref err)
-                        if err.kind() == io::ErrorKind::PermissionDenied => {
-                        eprintln!("{}", err);
-                        eprintln!("fatal: adding files failed");
-
-                        repo.index.release_lock()?;
-                        std::process::exit(128);
-                    },
-                    _ => {
-                        panic!("fatal: adding files failed");
-                    },
-                };
-                let stat = repo.workspace.stat_file(&pathname)?;
-
-                let blob = Blob::new(data.as_bytes());
-                repo.database.store(&blob)?;
-                
-                repo.index.add(&pathname, &blob.get_oid(), &stat);
-            }
-
-            repo.index.write_updates()?;
-            
-            Ok(())
-
-        },
-        _ => panic!("invalid command: {}", command),
     }
 }
